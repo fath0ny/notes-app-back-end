1389964bf24f286da2cbd4ac69715654df022551
fath0ny <arief.fathony10@gmail.com>
2026-09-22 14:16:52 +0700

    fixing bugs in createNote controller

--- src/controller.js
+++ src/controller.js
@@ -4,14 +4,14 @@ import notes from './notes.js';
 export const createNote = (req, res, next) => {
   const { title = 'untitled', tags, body } = req.body;
   const id = nanoid(16);
-  const createdAt = new Date().toISOString();
-  const updateAt = createdAt;
 
-  const newNote = { title, tags, body, id, createdAt, updateAt };
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = { title, tags, body, id, createdAt, updatedAt };
   notes.push(newNote);
 
   const isSuccess = notes.filter((note) => note.id === id).length > 0;
-
   if (isSuccess) {
     return res.status(201).json({
       status: 'success',
@@ -21,10 +21,9 @@ export const createNote = (req, res, next) => {
   }
 
   return res.status(500).json({
-    status: 'failed',
+    status: 'fail',
     message: 'Catatan gagal ditambahkan'
   });
-
 };
 
 export const getNotes = (req, res) => {
